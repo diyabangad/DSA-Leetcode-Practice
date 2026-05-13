e4d240ac7786f5b5ea7256b8cf12578d6f16ae21
diya bangad <diyabangad@gmail.com>
2026-05-13 16:50:07 +0530

    Create 001_largest_element.py

---
+++ arrays/easy/001_largest_element.py
@@ -0,0 +1,10 @@
+#diyabangad
+#Array - easy - 1 
+#Largest Element in an Array
+#GFG 
+def largest(arr, n):
+    maxi = arr[0]
+    for i in range(1, n):
+        if arr[i] > maxi:
+            maxi = arr[i]
+    return maxi
